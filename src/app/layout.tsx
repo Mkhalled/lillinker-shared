c643df0bf980a4918f51b43630e21925ef431819
anashm <anashammaoui07@gmail.com>
2025-08-22 11:19:02 +0100

    remove loader provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'jsvectormap/dist/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
 import '../css/satoshi.css';
 import '../css/style.css';
-import './globals.css';
+import "./globals.css";
 
+import Loader from '@/components/common/Loader';
 import { Providers } from '@/components/providers';
 
 const RootLayout = ({
@@ -13,11 +14,19 @@ const RootLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // const pathname = usePathname();
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
         <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          <Providers>{children}</Providers>
+          {loading ? <Loader /> : <Providers>{children}</Providers>}
         </div>
       </body>
     </html>
